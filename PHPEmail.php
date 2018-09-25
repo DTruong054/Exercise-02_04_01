094d96601e481184db6a447b054f0b33519be33c
Unknown <DTruong054@west-mec.org>
2018-09-25 15:20:59 -0700

    Psuhed

---
+++ PHPEmail.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<!-- 
+    Exercise-02_01_01
+    Author: Daniel Truong
+    Date: 9.10.18
+    File: DiceRoll.php -->
+
+<head>
+    <meta charset="utf-8" />
+    <title>Email</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="modernizr.custom.65897.js"></script>
+</head>
+
+<body>
+    <h2>Email</h2>
+    <?php
+        print "Hello world"
+    ?>
+</body>
+
+</html>
