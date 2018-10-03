c312862d6275a96b6fdd952a586f3ea9502860e0
Unknown <DTruong054@west-mec.org>
2018-10-03 07:24:31 -0700

    Meep
    Mep

--- processjumbleMaker.php
+++ processjumbleMaker.php
@@ -11,55 +11,45 @@
 <body>
     <h2>Process Jumble Maker</h2>
     <?php
-        $errorCount = 0;
-        $words = array();
-
-        function displayError($fieldName, $errorMsg)
-        {
-            global $errorCount;
-            echo "Error for \"$fieldName\": $errorMsg<br>\n";
-            ++$errorCount;
-        }
-
-        function validateWord($data, $fieldName)
-        {
-            global $errorCount;
-            $retval = "";
-            if (empty($data)) {
-                displayError($fieldName, "This Field is required");
+            $errorCount = 0;
+            $words = array();
+            function displayError($fieldName, $errorMsg) {
+                global $errorCount;
+                echo "Error for \"$fieldName\":$errorMsg<br>\n";
                 ++$errorCount;
+            }
+            function validateWord($data, $fieldName) {
+                global $errorCount;
                 $retval = "";
-            } else{
-                $retval = trim($data);
-                $retval = stripslashes($retval);
-                if (strlen($retval) < 4 || strlen($retval) > 7) {
-                    displayError($fieldName, "Words must be between 4 and 7 characters long");
+                if (empty($data)) {
+                    displayError($fieldName, "This field is required");
+                    ++$errorCount;
+                    $retval = "";
                 }
-                if (preg_match("/^[A-Za-z]+$/i", $retval) == 0) {
-                    displayError($fieldName, "Words must consist only of letters");
+                else {
+                    $retval = trim($data);
+                    $retval = stripslashes($retval);
+                    if (strlen($retval) < 4 || strlen($retval) > 7) {
+                        displayError($fieldName, "Words must consist only of letters");
+                    }
+                }
+                $retval = strtoupper($retval);
+                $retval = str_shuffle($retval);
+                return $retval;
+            }
+            $words[] = validateWord($_POST['word1'], "Word 1");
+            $words[] = validateWord($_POST['word2'], "Word 2");
+            $words[] = validateWord($_POST['word3'], "Word 3");
+            $words[] = validateWord($_POST['word4'], "Word 4");
+            if ($errorCount > 0) {
+                echo "Please use the \"Back\" button to re-enter any missing data.<br>\n";
+            }
+            else {
+                $wordNum = 0;
+                foreach ($words as $word) {
+                    echo "Word " . ++$wordNum . ": $word<br>\n";
                 }
             }
-            $retval = strtoupper($retval);
-            $retval = str_shuffle($retval);
-            return $retval;
-            return $data;
-            $words[] = $validateWord($_POST['word1'], "Word 1");
-            $words[] = $validateWord($_POST['word2'], "Word 2");
-            $words[] = $validateWord($_POST['word3'], "Word 3");
-            $words[] = $validateWord($_POST['word4'], "Word 4");   
-        }
-
-        
-        if ($errorCount) {
-            echo 'Please use the "Back" button to re-enter any missing data';
-        } else {
-            $wordNum = 0;
-            foreach ($words as $word) {
-                echo 'Word ' . ++$wordNum . ": $word<br>\n";
-            }
-        }
-        
-        
-    ?>
+        ?>
 </body>
 </html>
