b0c51751ece20adb38735f5fafc2a4902625269e
Unknown <DTruong054@west-mec.org>
2018-10-09 13:39:29 -0700

    Hmmm

--- PayCheck.php
+++ PayCheck.php
@@ -57,23 +57,42 @@
         background: rgb(60,179,113);
     }
 </style>
-    <?php
+<section id="div1">
+<?php
     //Getting stuff from HTML
         $hourWorked = $_POST['work'];
         $wage = $_POST['wage'];
         $pastLim = ($hourWorked - 40) * ($wage / 2);
 
-
-        if ($hourWorked > 40) {
-            //If hours are more then 40...
-            $overtime = number_format((($hourWorked * $wage) + ($pastLim)), 2);
-            echo "<p>You were paid $overtime for your work</p>" . "<br>";
-        } else {
-            //If hours are less then 40...
-            $normal = $wage * $hourWorked;
-            echo "<p>You were paid $normal for your work</p>" . "<br>";
+        if (!is_numeric($hourWorked) && !is_numeric($wage)) {
+            //Tests if real number or letter
+            echo "Please enter a real number";
+        }
+        else{
+            if (empty($hourWorked) && empty($wage)) {
+                //If the hours worked and the wage is empty...
+                echo "Please click the back button and enter the hours worked and the wage.";
+            } else if (empty($hourWorked)){
+                //If teh hours worked is empty...
+                echo "Please click the back button and enter the hours you have worked.";
+            } else if (empty($wage)){
+                //if the wage is empty
+                echo "Please click the back button and enter the wage.";
+            } else {
+                //If hoursworked and wage is filled out.
+                if ($hourWorked > 40) {
+                    //If hours are more then 40...
+                    $overtime = number_format((($hourWorked * $wage) + ($pastLim)), 2);
+                    echo "<p>You were paid \$$overtime for your work</p>" . "<br>";
+                } else {
+                    //If hours are less then 40...
+                    $normal = $wage * $hourWorked;
+                    echo "<p>You were paid \$$normal for your work</p>" . "<br>";
+                }
+            }
         }
     ?>
     <form action="PayCheck.html"><input type="submit" name="back" value="Back" class="button"></form>
+</section>
 </body>
 </html>
