8ae7f6840a6b3538c5c58af263e15425bd164de9
Unknown <DTruong054@west-mec.org>
2018-10-10 14:25:59 -0700

    Resolve sponge fancy
    Grip box distance

--- PayCheck.php
+++ PayCheck.php
@@ -16,83 +16,67 @@
         background-color: #808080;
     }
 
+    section{
+        background-color: rgb(105,105,105);
+        border-radius: 100px;
+    }
+
     #div1{
         border-radius: 100px;
     }
 
-    h2{
+    #p1{
         text-align: center;
-        background: rgb(60,179,113);
-        margin-bottom: 0px;
-        padding: 1.1em;
-        border-radius: 100px 100px 0px 0px;
     }
 
     form{
-        background-color: rgb(105,105,105);
-        margin-top: 0px;
-        border-radius: 0px 0px 100px 100px;
-    }
-
-    #p1{
         text-align: center;
-        margin-top: 0px;
-    }
-
-    #p2{
-        text-align: center;
-    }
-
-    #div2{
-        text-align: center;
-    }
-
-    p{
-        margin-bottom: 0px;
-        background: rgb(105,105,105);
-        padding-bottom: 0px;
     }
 
     .button{
         background: rgb(60,179,113);
+        text-align: center;
+        padding: 10px 50px;
     }
 </style>
-<section id="div1">
-<?php
-    //Getting stuff from HTML
+    <section>
+        <?php
+        //Getting stuff from HTML
         $hourWorked = $_POST['work'];
         $wage = $_POST['wage'];
         $pastLim = ($hourWorked - 40) * ($wage / 2);
 
         if (!is_numeric($hourWorked) && !is_numeric($wage)) {
             //Tests if real number or letter
-            echo "Please enter a real number";
+            echo "<p id='p1'>Please enter a real number</p>";
         }
         else{
             if (empty($hourWorked) && empty($wage)) {
                 //If the hours worked and the wage is empty...
-                echo "Please click the back button and enter the hours worked and the wage.";
+                echo "<p id='p1'>Please click the back button and enter the hours worked and the wage.</p>";
             } else if (empty($hourWorked)){
                 //If teh hours worked is empty...
-                echo "Please click the back button and enter the hours you have worked.";
+                echo "<p id='p1'>Please click the back button and enter the hours you have worked.</p>";
             } else if (empty($wage)){
                 //if the wage is empty
-                echo "Please click the back button and enter the wage.";
-            } else {
+                echo "<p id='p1'>Please click the back button and enter the wage.</p>";
+            } else if (is_numeric($hourWorked) && is_numeric($wage)){
                 //If hoursworked and wage is filled out.
                 if ($hourWorked > 40) {
                     //If hours are more then 40...
                     $overtime = number_format((($hourWorked * $wage) + ($pastLim)), 2);
-                    echo "<p>You were paid \$$overtime for your work</p>" . "<br>";
+                    echo "<p id='p1'>You were paid \$$overtime for your work</p>" . "<br>";
                 } else {
                     //If hours are less then 40...
                     $normal = $wage * $hourWorked;
-                    echo "<p>You were paid \$$normal for your work</p>" . "<br>";
+                    echo "<p id='p1'>You were paid \$$normal for your work</p>" . "<br>";
                 }
+            } else {
+                echo "<p id='p1'>Please don't combine numbers and letters</p>";
             }
         }
-    ?>
+        ?>
     <form action="PayCheck.html"><input type="submit" name="back" value="Back" class="button"></form>
-</section>
+    </section>
 </body>
 </html>
